3252fbf6b4550b9bc80b14a72a2f5acfef077682
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2025-04-04 17:03:56 +0300

    0.2.8 default

--- pipe.sh
+++ pipe.sh
@@ -34,7 +34,7 @@ DISK=150
 RAM=8
 LATEST_VERSION=$(. <(wget -qO- https://raw.githubusercontent.com/mgpwnz/pipe-pop/refs/heads/main/ver.sh))
 LOG_VERSION=$(journalctl -n 100 -u pop -o cat | grep -oP 'Latest version:\s*\K[0-9]+\.[0-9]+\.[0-9]+' | tail -1)
-DEF_VERSION=0.2.2
+DEF_VERSION=0.2.8
 #CURRENT_VERSION=$($HOME/opt/dcdn/pop --version | awk '/[0-9]+\.[0-9]+\.[0-9]+/ {for(i=1;i<=NF;i++) if ($i ~ /^[0-9]+\.[0-9]+\.[0-9]+$/) print $i}')
 CURRENT_VERSION=$($HOME/opt/dcdn/pop --version 2>/dev/null | awk '/[0-9]+\.[0-9]+\.[0-9]+/ {for(i=1;i<=NF;i++) if ($i ~ /^[0-9]+\.[0-9]+\.[0-9]+$/) print $i}')
 
